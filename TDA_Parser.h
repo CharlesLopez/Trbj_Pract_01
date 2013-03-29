7c9c68b82cf37b5d0f08a6f557eacfccb53450e0
Charles López <charlesklp2012@gmail.com>
2013-03-29 18:30:53 -0300

    Agrego archivo TDA_Perser.h terminado

---
+++ TDA_Parser.h
@@ -0,0 +1,31 @@
+# ifndef __TDA_Parser_h__
+# define __TDA_Parser_h__
+# define MAX_CAR 60
+
+typedef struct {
+    char    Nombre[20]= "Nombre Vacio";
+    char    Jugada01[2];
+    char    Jugada02[2];
+    char    Jugada03[2];
+    char    Jugada04[2];
+    char    Jugada05[2];
+    char    Jugada06[2];
+    char    Jugada07[2];
+    char    Jugada09[2];
+    char    Jugada10[3];
+    int     NumLinea = 0;
+} LineaArchivo;
+
+typedef struct {
+    int NLA;
+    LineaArchivo Archivo[NLA];
+} TDA_Parser;
+
+// Primitivas.
+
+int parserCrear (TDA_Parser *tda_parser, char[40] arch, char separador);
+int obtenerSiguienteLinea (TDA_Parser *tda_parser,);
+int obtenerCampo (TDA_Parser tda_parser, int N, char *Valor);
+int destruir (TDA_Parser *tda_parser);
+
+# endif
