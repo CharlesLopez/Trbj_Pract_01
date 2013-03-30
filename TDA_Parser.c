b493feb70c240305dbd955607be8ccf1c95e8850
Charles López <charlesklp2012@gmail.com>
2013-03-30 02:14:01 -0300

    Inicia definición de Primitiva parcerCrear

--- TDA_Parser.c
+++ TDA_Parser.c
@@ -1,7 +1,19 @@
+# include <stdio.h>
 # include "TDA_Parser.h"
 
 int parserCrear (TDA_Parser *tda_parser, char[40] arch, char separador)
 {
+    int Valor;
+    int I;
+    FILE *P_Archivo;
+    P_Archivo = fopen(arch,r);
+    rewind(P_Archivo);
+
+    for(I=1,!feof(P_Archivo), I++);
+    {
+
+    }
+
 
 }
 
