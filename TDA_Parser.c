8cc84c44552acfd1ff02139e29faa06c7fb3dc4e
Charles López <charlesklp2012@gmail.com>
2013-03-29 21:32:11 -0300

    Agrego archivo TDA_Parser.c

--- TDA_Parser.c
+++ TDA_Parser.c
@@ -1,5 +1,21 @@
-void main()
+# include "TDA_Parser.h"
+
+int parserCrear (TDA_Parser *tda_parser, char[40] arch, char separador)
 {
-	int A = 0;
-	return A;
+
+}
+
+int obtenerSiguienteLinea (TDA_Parser *tda_parser,)
+{
+
+}
+
+int obtenerCampo (TDA_Parser tda_parser, int N, char *Valor)
+{
+
+}
+
+int destruir (TDA_Parser *tda_parser)
+{
+
 }
