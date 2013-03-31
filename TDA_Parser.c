041de06b2e3419c11c1661725d5b151994324ded
Charles López <charlesklp2012@gmail.com>
2013-03-31 13:18:07 -0300

    Separo las estructuras de las primitivas

--- TDA_Parser.c
+++ TDA_Parser.c
@@ -1,40 +1,94 @@
-# include <stdio.h>
-# include "TDA_Parser.h"
+#include <stdio.h>
+#include "TDA_Parser.h"
 
 int parserCrear (TDA_Parser *Tda_Parser, char[40] Arch, char Separador)
 {
+    char CadeAux;
     int I;
     FILE *P_Archivo;
-    P_Archivo = fopen(arch,r);
-    rewind(P_Archivo);
 
-    char LetraAux;
-    for(I=1,!feof(P_Archivo), I++);
+    P_Archivo = fopen(Arch,r);
+
+    if (!P_Archivo)
     {
-        for(J=0, )
-            if ((fgetc(P_Archivo)!= "\o") and (fgetc(P_Archivo)!= "Separador"))
-                {
-                    LetraAux = fgetc(P_Archivo);
-                    Tda_Parser.
+        printf("El Archivo: -> %s"," NO EXISTE y NO FUE ABIERTO", Arch);
+        return 0;
+    }
+    else
+        rewind(P_Archivo);
 
-                }
-
+    if (feof(P_Archivo))
+    {
+        printf("El Archivo: -> %s"," ESTA VACIO!!!!!!", Arch);
+        return 0;
     }
 
+    else
+    {
+      for(I= 1, feof(P_Archivo), I++)
+      {
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Nombre= Tda_Parser[I-1].Nombre + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada01= Tda_Parser[I-1].Jugada01 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada02= Tda_Parser[I-1].Jugada02 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada03= Tda_Parser[I-1].Jugada03 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada04= Tda_Parser[I-1].Jugada04 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada05= Tda_Parser[I-1].Jugada05 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada06= Tda_Parser[I-1].Jugada06 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada07= Tda_Parser[I-1].Jugada07 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada08= Tda_Parser[I-1].Jugada08 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada09= Tda_Parser[I-1].Jugada09 + CadeAux;
+          do
+            CadeAux = fgetc(P_Archivo);
+          while(CadeAux!= Separador);
+          Tda_Parser[I-1].Jugada10= Tda_Parser[I-1].Jugada10 + CadeAux;
+          Tda_Parser[I-1].NumLinea= I;
+    }
+      return 1;
+    }
+}
+
+int obtenerSiguienteLinea (TDA_Parser *Tda_Parser,)
+{
+    int ContLinea;
 
 }
 
-int obtenerSiguienteLinea (TDA_Parser *tda_parser,)
+int obtenerCampo (TDA_Parser Tda_Parser, int N, char *Valor)
 {
 
 }
 
-int obtenerCampo (TDA_Parser tda_parser, int N, char *Valor)
-{
-
-}
-
-int destruir (TDA_Parser *tda_parser)
+int destruir (TDA_Parser *Tda_Parser)
 {
 
 }
--- TDA_Parser.h
+++ TDA_Parser.h
@@ -2,25 +2,11 @@
 # define __TDA_Parser_h__
 # define MAX_CAR 60
 
-typedef struct {
-    char    Nombre[20]= "Nombre Vacio";
-    char    Jugada01[2];
-    char    Jugada02[2];
-    char    Jugada03[2];
-    char    Jugada04[2];
-    char    Jugada05[2];
-    char    Jugada06[2];
-    char    Jugada07[2];
-    char    Jugada09[2];
-    char    Jugada10[3];
-    int     NumLinea = 0;
-} TDA_Parser;
-
 // Primitivas.
 
-int parserCrear (TDA_Parser *tda_parser, char[40] arch, char separador);
-int obtenerSiguienteLinea (TDA_Parser *tda_parser,);
-int obtenerCampo (TDA_Parser tda_parser, int N, char *Valor);
-int destruir (TDA_Parser *tda_parser);
+int parserCrear (TDA_Parser *Tda_Parser, char[40] Arch, char Separador);
+int obtenerSiguienteLinea (TDA_Parser *Tda_Parser,);
+int obtenerCampo (TDA_Parser Tda_Parser, int N, char *Valor);
+int destruir (TDA_Parser *Tda_Parser);
 
 # endif
