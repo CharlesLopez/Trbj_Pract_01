fdbc739396dfe74894643ec694d7dbe5ddd7f084
Francisco P. <pirra_23@hotmail.com>
2013-03-30 23:44:32 -0300

    TDA_Score.c : Esqueleto de funciones

---
+++ TDA_Score.c
@@ -0,0 +1,29 @@
+# Include <stdio.h>
+# Include "TDA_Score.h"
+
+
+
+int crear(TDA_Score* score) {
+
+}
+
+
+int AgregarTiro(TDA_Score* score, char[3] detalle, int posicion) {
+
+}
+
+int calcularResultado(TDA_Score score){
+
+}
+
+int establecerParticipante(TDA_Score* score, char[20] nombreUsuario) {
+
+}
+
+char[20] obtenerParticipante(TDA_Score score) {
+
+}
+
+void destruir(TDA_Score* score){
+
+}
