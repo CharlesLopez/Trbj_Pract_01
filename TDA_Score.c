62be6fcb4f55f84bb49d277c6f5617cfdc8a9aaf
Francisco P. <pirra_23@hotmail.com>
2013-03-31 23:26:23 -0300

    Primitiva: EstablecerParticipante

--- TDA_Score.c
+++ TDA_Score.c
@@ -1,6 +1,7 @@
 # Include <stdio.h>
 # Include "TDA_Score.h"
 
+// Valores de retorno: Falla=0 ; Exito=1
 
 
 int crear(TDA_Score* score) {
@@ -17,11 +18,12 @@ int calcularResultado(TDA_Score score){
 }
 
 int establecerParticipante(TDA_Score* score, char nombreUsuario[20]) {
-
+    TDA_Score.nombre=nombreUsuario;
+    Return 1;
 }
 
-char obtenerParticipante[20](TDA_Score score) {
-     Return TDA_Score.Nombre;                    /* Devuelve el nombre del jugador, almacenado en el tda_Score*/
+char obtenerParticipante[20](TDA_Score score) {
+    Return TDA_Score.Nombre;                    /* Devuelve el nombre del jugador, almacenado en el tda_Score*/
 }
 
 void destruir(TDA_Score* score){
