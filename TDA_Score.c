8df56fce60bd8471c9825786fe601d96ef5499ae
Francisco P. <pirra_23@hotmail.com>
2013-04-02 19:33:47 -0300

    TDA score:Primitva calcular puntos y modif struct

--- TDA_Score.c
+++ TDA_Score.c
@@ -3,49 +3,91 @@
 #include "TDA_Score.h"
 
 
-// Valores de retorno: Falla=0 ; Exito=1
+// Valores de retorno: Falla=0 ; Exito>0
 
 
 int crear(TDA_Score *score) {
     score.nombre="";
     int i;
-    for (i=1;i<11;i++) {
-            score.jugada[i][0]='';
-            score.jugada[i][1]='';
-            score.jugada[i][2]='';
-            score.jugada[i][3]='';
+    for (i=0;i<11;i++) {
+            score.jugada[i][0]=0;
+            score.jugada[i][1]=0;
+            score.jugada[i][2]=0;
     }
     return 1;
 }
 
 
 int AgregarTiro(TDA_Score *score,char detalle[4],int posicion) {
-    score.jugada[posicion][0]=detalle[0];
-    score.jugada[posicion][1]=detalle[1];
-    score.jugada[posicion][2]=detalle[2];
-    score.jugada[posicion][3]=detalle[3];
+    int i;
+    for (i=0,i<4,i++){
+        switch (detalle[i]){
+            case '1': score.jugada[posicion][i]=1; break;
+            case '2': score.jugada[posicion][i]=2; break;
+            case '3': score.jugada[posicion][i]=3; break;
+            case '4': score.jugada[posicion][i]=4; break;
+            case '5': score.jugada[posicion][i]=5; break;
+            case '6': score.jugada[posicion][i]=6; break;
+            case '7': score.jugada[posicion][i]=7; break;
+            case '8': score.jugada[posicion][i]=8; break;
+            case '9': score.jugada[posicion][i]=9; break;
+            case 'x': score.jugada[posicion][i]=-1; break;      // Las x las transformo en -1 y las barras en -2
+            case '/': score.jugada[posicion][i]=-2; break;
+        }
+    }
     return 1;
 }
 
-int calcularResultado(TDA_Score score){
-    int aux1,cont,i,j,strikes;
-    for(i=1;i<11;i++){
-        strikes=0;
-        for(j=1;i<4;i++){
-            switch (score.jugada[i][j]){
-                case '1': aux1=1; cont=cont+aux1; break;
-                case '2': aux1=2; cont=cont+aux1; break;
-                case '3': aux1=3; cont=cont+aux1; break;
-                case '4': aux1=4; cont=cont+aux1; break;
-                case '5': aux1=5; cont=cont+aux1; break;
-                case '6': aux1=6; cont=cont+aux1; break;
-                case '7': aux1=7; cont=cont+aux1; break;
-                case '8': aux1=8; cont=cont+aux1; break;
-                case '9': aux1=9; cont=cont+aux1; break;
+long calcularResultado(TDA_Score score){
+    int i,j,h,k,puntos[11];
+    long total;
+
+    for (k=1;k<11;k++){
+            puntos[k]=0;
+    }
+
+    for(i=1;i<10;i++){
+        if(score.jugada[i][0]==-1){
+                puntos[i]=10;
+                if(score.jugada[i-1][0]==-1){
+                        puntos[i-1]=puntos[i-1]+10;
+                        if (i>2){
+                            if(score.jugada[i-2][0]==-1){
+                                puntos[i-2]=puntos[i-2]+10;
+                            }
+                        }
+                }
+                if(score.jugada[i-1][1]==-2){
+                        puntos[i-1]=puntos[i-1]+10;
+                }
+        }
+
+        if(score.jugada[i][1]==-2){
+                puntos[i]=10;
+        }else{
+            puntos[i]=score.jugada[i][0]+score.jugada[i][1];
+            if(score.jugada[i-1][0]==-1){
+                puntos[i-1]=puntos[i-1]+puntos[i];
+            }
+            if(score.jugada[i-1][1]==-2){
+                puntos[i-1]=puntos[i-1]+score.jugada[i][0];
             }
         }
     }
-    return cont;
+
+    for(j=0;j<4;j++){
+        switch (score.jugada[j]){
+            case -1: score.jugada[10][j]=10; break;
+            case -2: score.jugada[10][j]=10; break;
+        }
+        puntos[10]=puntos[10]+score.jugada[10][j];
+    }
+
+    total=0;
+    for (h=1;h<11;h++){
+        total=total+puntos[h];
+    }
+    return total;
 }
 
 int establecerParticipante(TDA_Score *score,char nombreUsuario[20]) {
@@ -60,5 +102,4 @@ char obtenerParticipante[20](TDA_Score score) {
 int destruir(TDA_Score *score){
     free(score);
     return 1;
-
 }
