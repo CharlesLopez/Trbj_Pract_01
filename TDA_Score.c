23dcb69a27d761f6210693ca61944af11db3e124
Francisco P. <pirra_23@hotmail.com>
2013-04-02 02:27:22 -0300

    TDA_Score: Seguimos con las correciones
    Cambie un par de cosas que tiraba error, me queda pendiente el error que
    da cuando accesa a score.nombre o score.jugada  y terminar el de
    calcular resultado

--- TDA_Score.c
+++ TDA_Score.c
@@ -5,9 +5,10 @@
 // Valores de retorno: Falla=0 ; Exito=1
 
 
-int crear(TDA_Score* score) {
+int crear(TDA_Score *score) {
     score.nombre="";
-    for (int i=1;i<11;i++) {
+    int i;
+    for (i=1;i<11;i++) {
             score.jugada[i][0]='';
             score.jugada[i][1]='';
             score.jugada[i][2]='';
@@ -17,7 +18,7 @@ int crear(TDA_Score* score) {
 }
 
 
-int AgregarTiro(TDA_Score* score,char detalle[4],int posicion) {
+int AgregarTiro(TDA_Score *score,char detalle[4],int posicion) {
     score.jugada[posicion][0]=detalle[0];
     score.jugada[posicion][1]=detalle[1];
     score.jugada[posicion][2]=detalle[2];
@@ -26,12 +27,26 @@ int AgregarTiro(TDA_Score* score,char detalle[4],int posicion) {
 }
 
 int calcularResultado(TDA_Score score){
-    for(int i=1;i<11;i++){
-
+    int aux1,cont,i,j;
+    for(i=1;i<11;i++){
+        for(j=1;i<4;i++){
+            switch (score.jugada[i][j]){
+                case '1': aux1=1;cont=cont+aux1;
+                case '2': aux1=2;cont=cont+aux1;
+                case '3': aux1=3;cont=cont+aux1;
+                case '4': aux1=4;cont=cont+aux1;
+                case '5': aux1=5;cont=cont+aux1;
+                case '6': aux1=6;cont=cont+aux1;
+                case '7': aux1=7;cont=cont+aux1;
+                case '8': aux1=8;cont=cont+aux1;
+                case '9': aux1=9;cont=cont+aux1;
+            }
         }
+    }
+    return cont;
 }
 
-int establecerParticipante(TDA_Score* score,char nombreUsuario[20]) {
+int establecerParticipante(TDA_Score *score,char nombreUsuario[20]) {
     TDA_Score.nombre=nombreUsuario;                  /*Inserta dentro del tdascore el nombre de usuario que recibio, supuestamente desde el parser*/
     return 1;
 }
@@ -40,7 +55,15 @@ char obtenerParticipante[20](TDA_Score score) {
     return TDA_Score.Nombre;                    /* Devuelve el nombre del jugador, almacenado en el tda_Score*/
 }
 
-int destruir(TDA_Score* score){
-    free(score);                              /* Libera la memoria usada por el score*/
+int destruir(TDA_Score *score){
+    score->nombre="";
+    int i;
+    for (i=1;i<11;i++) {
+            score.jugada[i][0]='';
+            score.jugada[i][1]='';
+            score.jugada[i][2]='';
+            score.jugada[i][3]='';
+    }
     return 1;
+
 }
