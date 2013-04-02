fae420ccbc4669d9c4f99723922f49c4ceb6d001
Francisco P. <pirra_23@hotmail.com>
2013-04-02 01:33:54 -0300

    Correccion de errores varios y cambio de struct

--- TDA_Score.c
+++ TDA_Score.c
@@ -1,54 +1,46 @@
 #include <stdio.h>
 #include "TDA_Score.h"
 
+
 // Valores de retorno: Falla=0 ; Exito=1
 
 
-int crear(TDA_Score* score) {
+int crear(TDA_Score* score) {
     score.nombre="";
-    for (int i=1;i<10;i++) {
-            score.jugada.i[0]="";
-            score.jugada.i[1]="";
-            score.jugada.i[2]="";
+    for (int i=1;i<11;i++) {
+            score.jugada[i][0]='';
+            score.jugada[i][1]='';
+            score.jugada[i][2]='';
+            score.jugada[i][3]='';
     }
-    score.jugada.10[0]="";
-    score.jugada.10[1]="";
-    score.jugada.10[2]="";
-    score.jugada.10[3]="";
     return 1;
 }
 
 
-int AgregarTiro(TDA_Score* score, char detalle[4], int posicion) {
-    if (posicion=10) {
-            score.jugada.10[0]=detalle[0];
-            score.jugada.10[1]=detalle[1];
-            score.jugada.10[2]=detalle[2];
-            return 1;
-            } else {
-                score.jugada.posicion[0]=detalle[0];
-                score.jugada.posicion[1]=detalle[1];
-                return 1;
-            }
-
+int AgregarTiro(TDA_Score* score,char detalle[4],int posicion) {
+    score.jugada[posicion][0]=detalle[0];
+    score.jugada[posicion][1]=detalle[1];
+    score.jugada[posicion][2]=detalle[2];
+    score.jugada[posicion][3]=detalle[3];
+    return 1;
 }
 
-int calcularResultado(TDA_Score score){
-    For(int i=1;i<11;i++){
+int calcularResultado(TDA_Score score){
+    for(int i=1;i<11;i++){
 
         }
 }
 
-int establecerParticipante(TDA_Score* score, char nombreUsuario[20]) {
+int establecerParticipante(TDA_Score* score,char nombreUsuario[20]) {
     TDA_Score.nombre=nombreUsuario;                  /*Inserta dentro del tdascore el nombre de usuario que recibio, supuestamente desde el parser*/
     return 1;
 }
 
-char obtenerParticipante[20](TDA_Score score) {
+char obtenerParticipante[20](TDA_Score score) {
     return TDA_Score.Nombre;                    /* Devuelve el nombre del jugador, almacenado en el tda_Score*/
 }
 
-void destruir(TDA_Score* score){
+int destruir(TDA_Score* score){
     free(score);                              /* Libera la memoria usada por el score*/
     return 1;
 }
--- TDA_Score.h
+++ TDA_Score.h
@@ -3,28 +3,19 @@
 
 
 typedef struct {
-    char Nombre[20];
-    struct {
-          char 1[3];
-          char 2[3];
-          char 3[3];
-          char 4[3];
-          char 5[3];
-          char 6[3];
-          char 7[3];
-          char 8[3];
-          char 9[3];
-          char 10[4];
-     } Jugada;
+    char nombre[20];
+    char jugada[10][4];  // Es un vector de vectores, para cada tiro tiene sus respectivos lugares
 } TDA_Score;
 
 // Primitivas
 
-int crear(TDA_Score* score); /*Recibe un TDA_Score y lo inicializa de manera correcta para su posterior uso, en caso de falla o éxito devuelve el código correspondiente*/
-int AgregarTiro(TDA_Score* score, char detalle[3], int posicion); /*Recibe un TDA_Score inicializado, un detalle del tiro (que puede tener uno o dos o tres caracteres) y la posición de ejecución (1 a 10), en caso de falla o éxito devuelve el código correspondiente.*/
-int calcularResultado(TDA_Score score); /*Recibe un TDA_Score inicializado, y devuelve la puntuación obtenida, en caso de falla devuelve un código entero negativo.*/
-int establecerParticipante(TDA_Score* score, char nombreUsuario[20]); /*Recibe un TDA_Score inicializado y establece el nombre de usuario recibido por parametro, en caso de falla o éxito devuelve el código correspondiente*/
-char obtenerParticipante[20](TDA_Score score); /*Recibe un TDA_Score inicializado y devuelve el nombre de usuario */
-void destruir(TDA_Score* score); /*Recibe un TDA_Score inicializado para el cual se liberan los recursos utilizados dejando al TDA inutilizable.*/
+int crear(TDA_Score* score);/*Recibe un TDA_Score y lo inicializa de manera correcta para su posterior uso, en caso de falla o éxito devuelve el código correspondiente*/
+int agregarTiro(TDA_Score* score,char detalle[3], int posicion); /*Recibe un TDA_Score inicializado, un detalle del tiro (que puede tener uno o dos o tres caracteres) y la posición de ejecución (1 a 10), en caso de falla o éxito devuelve el código correspondiente.*/
+int calcularResultado(TDA_Score score); /*Recibe un TDA_Score inicializado, y devuelve la puntuación obtenida, en caso de falla devuelve un código entero negativo.*/
+int establecerParticipante(TDA_Score* score,char nombreUsuario[20]); /*Recibe un TDA_Score inicializado y establece el nombre de usuario recibido por parametro, en caso de falla o éxito devuelve el código correspondiente*/
+char obtenerParticipante[20](TDA_Score score); /*Recibe un TDA_Score inicializado y devuelve el nombre de usuario */
+int destruir(TDA_Score* score); /*Recibe un TDA_Score inicializado para el cual se liberan los recursos utilizados dejando al TDA inutilizable.*/
 
 #endif // __score_h__
+
+
